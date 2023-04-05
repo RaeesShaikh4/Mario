3d720687eaf228cf89787e2a75d66ce8d2aca502
Raees Nawaj Shaikh <112468174+RaeesShaikh4@users.noreply.github.com>
2023-04-05 20:03:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+score = 0;
+cross = true;
+
+playMusic = document.getElementById('playMusic');
+
+
+
+audio = new Audio('marioBG.mp3');
+audiogo = new Audio('gameover.wav');
+up = new Audio('up.wav')
+
+playMusic.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        document.getElementById("playMusic").src = "volume-off-solid.svg";
+        } else {
+        audio.pause();
+        document.getElementById("playMusic").src = "volume-high-solid.svg";
+
+    }
+})
+
+document.onkeydown = function (e) {
+    console.log(`Key Code is ${e.keyCode}`)
+    if (e.keyCode == 38) {
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        up.play();
+        setTimeout(() => {
+            dino.classList.remove('animateDino');
+        }, 800)
+    }
+    if (e.keyCode == 39) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+    if (e.keyCode == 37) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
+    }
+}
+
+
+setInterval(() => {
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacles');
+
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+
+
+    if (offsetX < 73 && offsetY < 52) {
+        gameOver.style.visibility = 'visible';
+        obstacle.classList.remove('obsAni');
+        audio.pause();
+        audiogo.play();
+
+        setTimeout(() => {
+            audiogo.pause();
+            audio.pause();
+        }, 3500);
+
+    } else if (offsetX < 80 && cross) {
+        score += 1;
+        updateScore(score);
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
+
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.1;
+            obstacle.style.animationDuration = newDur + 's';
+        }, 500)
+    }
+
+
+}, 10)
+
+function updateScore(score) {
+    scoreCount.innerHTML = `Your score : ${score}`;
+}
+
